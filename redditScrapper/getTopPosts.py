bc3ece2ca331f77fac0eb85ad72ee4b8d3597603
Niraj Kumar <nirajk23523@gmail.com>
2023-03-03 17:25:50 +0530

    experimentation

--- redditScrapper/getTopPosts.py
+++ redditScrapper/getTopPosts.py
@@ -1,6 +1,7 @@
 import praw
+from praw.models import MoreComments
 import os
-# import pandas as pd
+import pandas as pd
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -14,11 +15,49 @@ reddit_read_only = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
 
 subreddit = reddit_read_only.subreddit('AskReddit')
 
-# Display the name of the Subreddit
-print("Display Name:", subreddit.display_name)
+posts = subreddit.top(time_filter="month")
+# Scraping the top posts of the current month
 
-# Display the title of the Subreddit
-print("Title:", subreddit.title)
+posts_dict = {"Title": [], "Post Text": [],
+              "ID": [], "Score": [],
+              "Total Comments": [], "Post URL": []
+              }
 
-# Display the description of the Subreddit
-print("Description:", subreddit.description)
+for post in posts:
+    # Title of each post
+    posts_dict["Title"].append(post.title)
+
+    # Text inside a post
+    posts_dict["Post Text"].append(post.selftext)
+
+    # Unique ID of each post
+    posts_dict["ID"].append(post.id)
+
+    # The score of a post
+    posts_dict["Score"].append(post.score)
+
+    # Total number of comments inside the post
+    posts_dict["Total Comments"].append(post.num_comments)
+
+    # URL of each post
+    posts_dict["Post URL"].append(post.url)
+
+# Saving the data in a pandas dataframe
+top_posts = pd.DataFrame(posts_dict)
+top_posts.to_csv("top_posts.csv", index=True)
+
+url = top_posts['Post URL'][0]
+submission = reddit_read_only.submission(url=url)
+
+# Getting the comments of the top post
+post_comments = []
+
+for comment in submission.comments:
+    if type(comment) == MoreComments:
+        continue
+
+    post_comments.append(comment.body)
+
+# creating a dataframe
+comments_df = pd.DataFrame(post_comments, columns=['comment'])
+comments_df.to_csv("comments.csv", index=True)
